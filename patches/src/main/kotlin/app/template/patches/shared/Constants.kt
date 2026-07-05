3a1f103234b4c21deb76f75eb76c5998bce4a3ed
rytek <0msbf6qjx@mozmail.com>
2026-07-05 23:03:56 +0330

    fix: update the constants to reflect the changes

--- patches/src/main/kotlin/app/template/patches/shared/Constants.kt
+++ patches/src/main/kotlin/app/template/patches/shared/Constants.kt
@@ -19,16 +19,11 @@ object Constants {
     )
 
     val COMPATIBILITY_EXAMPLE = Compatibility(
-        name = "XYZ app", // App name as it appears in the Android launcher.
+        name = "XYZ app",
         packageName = "com.example.app",
-        apkFileType = ApkFileType.APK, // Preferred or recommended file type.
-        appIconColor = 0xFF0045, // Icon color in Morphe Manager. Usually the same color as the icon background.
+        apkFileType = ApkFileType.APK,
+        appIconColor = 0xFF0045,
         targets = listOf(
-            // "version = null" means the patch works with the latest app target
-            // and is expected to work with all future app targets.
-            //
-            // It is highly recommended to always include the exact app version you developed your patches for
-            // or the last version you have confirmed as 100% working.
             AppTarget(
                 version = "2.0.0"
             ),
@@ -38,19 +33,16 @@ object Constants {
         )
     )
 
-
     val COMPATIBILITY_EXAMPLE_2 = Compatibility(
         name = "ABC app",
         packageName = "com.example.app",
         apkFileType = ApkFileType.APKM,
-        appIconColor = 0x00FF45, // Icon color in Morphe Manager. Usually the same color as the icon background.
+        appIconColor = 0x00FF45,
         targets = listOf(
-            // 'any' version supported experimentally.
             AppTarget(
                 version = null,
                 isExperimental = true
             ),
-            // App version confirmed 100% working.
             AppTarget(
                 version = "1.0.2"
             )
@@ -69,4 +61,4 @@ object Constants {
             )
         )
     )
-
+}
